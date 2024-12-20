b007244651a0c8c6f77bf807d322e5999218eb92
McKathlin <mckathlin@gmail.com>
2024-12-20 17:56:36 +0000

    Log full and simplified Indianpolis weather info

--- weather.js
+++ weather.js
@@ -1,1 +1,47 @@
-console.log("This script needs to fetch weather data.");
+async function fetchWeatherIn (locationStr) {
+  const BASE_PATH = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline';
+  const MY_FREE_KEY = 'J9RKTKAX2HRTXCXKVAQE4DERZ';
+  const RESPONSE_STATUS_OK = 200;
+
+  const location = encodeURIComponent(locationStr);
+  const INCLUDES = [
+    'current',
+    'days', 
+    'hours', 
+    'alerts',
+  ];
+  const INCLUDES_STR = INCLUDES.join(',');
+  const query = `${BASE_PATH}/${location}?key=${MY_FREE_KEY}&include=${INCLUDES_STR}`;
+
+  const response = await fetch(query);
+  if (response.status != RESPONSE_STATUS_OK) {
+    throw new Error(`HTTP ${response.status} response`);
+  }
+  const report = await response.json();
+  
+  console.log(report);
+  return simplifyWeather(report);
+}
+
+function simplifyWeather (report) {
+  const today = report.days[0];
+  return {
+    conditions: today.conditions.split(', '),
+    description: today.description,
+    highTemp: today.tempmax,
+    lowTemp: today.tempmin,
+    windDirection: today.winddir,
+    windSpeed: today.windspeed,
+  };
+}
+
+fetchWeatherIn('Indianapolis').then((simpleWeather) => {
+  console.log("Simplified weather report:", simpleWeather);
+});
+
+// winddir key:
+// Direction is in degrees clockwise, starting from north-sourced.
+// 0 is from due north V (heading southward)
+// 90 is from due west > (heading eastward)
+// 180 is from due south ^ (heading northward)
+// 270 is from due east < (heading westward)

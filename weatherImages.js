213fb02bf930af7f6bb0e7692d64be504f478142
McKathlin <mckathlin@gmail.com>
2024-12-31 02:43:08 +0000

    Create list of weather condition images as weatherImages.js

---
+++ weatherImages.js
@@ -0,0 +1,82 @@
+const weatherImages = {
+  clear: [
+    {
+      filename: "clear-sky-1.jpg",
+      credit: "McKathlin",
+    },
+    {
+      filename: "clear-sky-2.jpg",
+      credit: "McKathlin",
+    },
+    {
+      filename: "clear-sky-3.jpg",
+      credit: "McKathlin",
+    },
+  ],
+  cloudy: [
+    {
+      filename: "cloudy-chris-bair.jpg",
+      credit: "Chris Bair",
+    },
+    {
+      filename: "cloudy-tobias-stonjeck.jpg",
+      credit: "Tobias Stonjeck",
+    }
+  ],
+  partlyCloudy: [
+    {
+      filename: "partly-cloudy-chuttersnap.jpg",
+      credit: "Chuttersnap",
+    },
+    {
+      filename: "partly-cloudy-eberhard-grossgasteiger.jpg",
+      credit: "Eberhard Grossgasteiger",
+    },
+    {
+      filename: "partly-cloudy-rodion-kutsaiev.jpg.jpg",
+      credit: "Rodion Kutsaiev",
+    },
+    {
+      filename: "partly-cloudy-sam-schooler.jpg",
+      credit: "Sam Schooler",
+    },
+  ],
+  rain: [
+    {
+      filename: "rain-alex-dukhanov.jpg",
+      credit: "Alex Dukhanov",
+    },
+    {
+      filename: "rain-max-bender.jpg",
+      credit: "Max Bender",
+    },
+    {
+      filename: "rain-nick-nice.jpg",
+      credit: "Nick Nice",
+    },
+  ],
+  snow: [
+    {
+      filename: "snow-aaron-burden.jpg",
+      credit: "Aaron Burden",
+    },
+    {
+      filename: "snow-aditya-vyas.jpg",
+      credit: "Aditya Vyas",
+    },
+    {
+      filename: "snow-hide-obara.jpg",
+      credit: "Hide Obara",
+    },
+  ],
+  storm: [
+    {
+      filename: "thunderstorm-david-moum.jpg",
+      credit: "David Moum",
+    },
+    {
+      filename: "thunderstorm-eberhard-grossgasteiger.jpg",
+      credit: "Eberhard Grossgasteiger",
+    },
+  ]
+}
